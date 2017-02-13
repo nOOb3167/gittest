9fe808a80e9fe110b67af45653ba6ab8b5463b41
nOOb3167 <nOOb3167@gmail.com>
2017-02-13 13:36:55 +0100

    gittest: ClntState code (still debugging)

--- gittest_serv/gittest_serv.cpp
+++ gittest_serv/gittest_serv.cpp
@@ -25,6 +25,8 @@
 * = Inferred size vs Explicit size for frame vector serialization =
 */
 
+#define GS_AUX_MARKER_STRUCT_IS_COPYABLE /* dummy (marker / documentation purpose) */
+
 #define GS_PORT 3756
 
 #define GS_SERV_AUX_ARBITRARY_TIMEOUT_MS 5000
@@ -45,6 +47,7 @@
 
 #define GS_ERR_CLEAN(THE_R) { r = (THE_R); GS_DBG_CLEAN; goto clean; }
 #define GS_GOTO_CLEAN() { GS_DBG_CLEAN; goto clean; }
+#define GS_ERR_CLEANSUB(THE_R) { r = (THE_R); GS_DBG_CLEAN; goto cleansub; }
 #define GS_GOTO_CLEANSUB() { GS_DBG_CLEAN; goto cleansub; }
 
 #define GS_FRAME_TYPE_SERV_AUX_INTERRUPT_REQUESTED 0
@@ -1204,7 +1207,622 @@ clean:
 	return r;
 }
 
+typedef std::pair<ENetHost *, ENetPeer *> gs_host_peer_pair_t;
+
+struct PacketWithOffset {
+	gs_packet_t mPacket;
+	uint32_t mOffsetSize;
+	uint32_t mOffsetObject;
+
+	GS_AUX_MARKER_STRUCT_IS_COPYABLE;
+};
+
+int aux_make_packet_with_offset(gs_packet_t Packet, uint32_t OffsetSize, uint32_t OffsetObject, PacketWithOffset *oPacketWithOffset) {
+	PacketWithOffset ret;
+	ret.mPacket = Packet;
+	ret.mOffsetSize = OffsetSize;
+	ret.mOffsetObject = OffsetObject;
+	if (oPacketWithOffset)
+		*oPacketWithOffset = ret;
+	return 0;
+}
+
+struct ClntState {
+	sp<git_repository *> mRepositoryT;
+
+	sp<gs_host_peer_pair_t> mConnection;
+
+	sp<git_oid> mTreeHeadOid;
+
+	sp<std::vector<git_oid> > mTreelist;
+	sp<std::vector<git_oid> > mMissingTreelist;
+
+	sp<std::vector<git_oid> > mMissingBloblist;
+	sp<PacketWithOffset>      mTreePacketWithOffset;
+
+	sp<std::vector<git_oid> > mWrittenBlob;
+	sp<std::vector<git_oid> > mWrittenTree;
+
+	GS_AUX_MARKER_STRUCT_IS_COPYABLE;
+};
+
+#define GS_CLNT_STATE_CODE_SET_ENSURE_NONUCF(PTR_VARNAME_CLNTSTATE, CODE, VARNAME_TMPSTATE, STATEMENTBLOCK) \
+	{ ClntState VARNAME_TMPSTATE;                                                                       \
+      if (!!clnt_state_cpy(& (VARNAME_TMPSTATE), (PTR_VARNAME_CLNTSTATE)))                              \
+        GS_ERR_CLEAN(9998);                                                                             \
+	  { STATEMENTBLOCK }                                                                                \
+	  if (!!clnt_state_code_ensure(& (VARNAME_TMPSTATE), (CODE)))                                       \
+	    GS_ERR_CLEAN(9999);                                                                             \
+	  if (!!clnt_state_cpy((PTR_VARNAME_CLNTSTATE), & (VARNAME_TMPSTATE)))                              \
+	    GS_ERR_CLEAN(9998); }
+
+int clnt_state_cpy(ClntState *dst, const ClntState *src) {
+	*dst = *src;
+	return 0;
+}
+
+int clnt_state_code(ClntState *State, uint32_t *oCode) {
+	int r = 0;
+	
+	int Code = 0;
+
+	if (! State->mRepositoryT)
+		{ Code = 0; goto s0; }
+	if (! State->mConnection)
+		{ Code = 1; goto s1; }
+	if (! State->mTreeHeadOid)
+		{ Code = 2; goto s2; }
+	if (! State->mTreelist || ! State->mMissingTreelist)
+		{ Code = 3; goto s3; }
+	if (! State->mMissingBloblist || ! State->mTreePacketWithOffset)
+		{ Code = 4; goto s4; }
+	if (! State->mWrittenBlob || ! State->mWrittenTree)
+		{ Code = 5; goto s5; }
+	if (true)
+		{ Code = 6; }
+
+s0:
+	if (State->mConnection)
+		GS_ERR_CLEAN(1);
+s1:
+	if (State->mTreeHeadOid)
+		GS_ERR_CLEAN(1);
+s2:
+	if (State->mTreelist || State->mMissingTreelist)
+		GS_ERR_CLEAN(1);
+s3:
+	if (State->mMissingBloblist || State->mTreePacketWithOffset)
+		GS_ERR_CLEAN(1);
+s4:
+	if (State->mWrittenBlob || State->mWrittenTree)
+		GS_ERR_CLEAN(1);
+s5:
+
+	if (oCode)
+		*oCode = Code;
+
+clean:
+
+	return r;
+}
+
+int clnt_state_code_ensure(ClntState *State, uint32_t WantedCode) {
+	int r = 0;
+
+	uint32_t FoundCode = 0;
+
+	if (!!(r = clnt_state_code(State, &FoundCode)))
+		GS_GOTO_CLEAN();
+
+	if (WantedCode != FoundCode)
+		GS_ERR_CLEAN(1);
+
+clean:
+
+	return r;
+}
+
+int clnt_state_0_noown(const char *ConfRepoTOpenPath, git_repository **oRepositoryT) {
+	int r = 0;
+
+	if (!!(r = aux_repository_open(ConfRepoTOpenPath, oRepositoryT)))
+		GS_GOTO_CLEAN();
+
+clean:
+
+	return r;
+}
+
+int clnt_state_1_noown(uint32_t ConfServPort, const char *ConfServHostName, ENetHost **oHost, ENetPeer **oPeer) {
+	int r = 0;
+
+	ENetAddress address = {};
+
+	if (!!(r = aux_enet_address_create_hostname(ConfServPort, ConfServHostName, &address)))
+		GS_GOTO_CLEAN();
+
+	if (!!(r = aux_host_connect(&address, GS_CONNECT_NUMRETRY, GS_CONNECT_TIMEOUT_MS, oHost, oPeer)))
+		GS_GOTO_CLEAN();
+
+clean:
+
+	return r;
+}
+
+int clnt_state_2_noown(
+	const char *ConfRefName, git_repository *RepositoryT, ENetHost *host, ENetPeer *peer, ServAuxData *ServAuxData,
+	git_oid *oTreeHeadOid)
+{
+	int r = 0;
+
+	std::string Buffer;
+	gs_packet_t Packet;
+	uint32_t Offset = 0;
+
+	git_oid CommitHeadOidT = {};
+	git_oid TreeHeadOidT = {};
+
+	if (!!(r = aux_frame_full_write_request_latest_commit_tree(&Buffer)))
+		GS_GOTO_CLEAN();
+	if (!!(r = aux_packet_full_send(host, peer, ServAuxData, Buffer.data(), Buffer.size(), 0)))
+		GS_GOTO_CLEAN();
+
+	if (!!(r = aux_host_service_one_type_receive(host, GS_RECEIVE_TIMEOUT_MS, &Packet)))
+		GS_GOTO_CLEAN();
+
+	if (! Packet)
+		GS_ERR_CLEAN(1);
+
+	if (!!(r = aux_frame_ensure_frametype(Packet->data, Packet->dataLength, Offset, &Offset, GS_FRAME_TYPE_DECL(RESPONSE_LATEST_COMMIT_TREE))))
+		GS_GOTO_CLEAN();
+
+	if (!!(r = aux_frame_read_size_ensure(Packet->data, Packet->dataLength, Offset, &Offset, GS_PAYLOAD_OID_LEN)))
+		GS_GOTO_CLEAN();
+
+	if (!!(r = aux_frame_read_oid(Packet->data, Packet->dataLength, Offset, &Offset, oTreeHeadOid)))
+		GS_GOTO_CLEAN();
+
+	if (!!(r = clnt_latest_commit_tree_oid(RepositoryT, ConfRefName, &CommitHeadOidT, &TreeHeadOidT)))
+		GS_GOTO_CLEAN();
+
+	if (git_oid_cmp(&TreeHeadOidT, oTreeHeadOid) == 0) {
+		char buf[GIT_OID_HEXSZ] = {};
+		git_oid_fmt(buf, &CommitHeadOidT);
+		printf("[clnt] Have latest [%.*s]\n", GIT_OID_HEXSZ, buf);
+	}
+
+clean:
+
+	return r;
+}
+
+int clnt_state_3_noown(
+	git_repository *RepositoryT, ENetHost *host, ENetPeer *peer, ServAuxData *ServAuxData,
+	git_oid *TreeHeadOid, std::vector<git_oid> *oTreelist, std::vector<git_oid> *oMissingTreelist)
+{
+	int r = 0;
+
+	std::string Buffer;
+	gs_packet_t Packet;
+	uint32_t Offset = 0;
+	uint32_t IgnoreSize = 0;
+
+	if (!!(r = aux_frame_full_write_request_treelist(&Buffer, TreeHeadOid->id, GIT_OID_RAWSZ)))
+		GS_GOTO_CLEAN();
+	if (!!(r = aux_packet_full_send(host, peer, ServAuxData, Buffer.data(), Buffer.size(), 0)))
+		GS_GOTO_CLEAN();
+
+	if (!!(r = aux_host_service_one_type_receive(host, GS_RECEIVE_TIMEOUT_MS, &Packet)))
+		GS_GOTO_CLEAN();
+
+	if (! Packet)
+		GS_ERR_CLEAN(1);
+
+	if (!!(r = aux_frame_ensure_frametype(Packet->data, Packet->dataLength, Offset, &Offset, GS_FRAME_TYPE_DECL(RESPONSE_TREELIST))))
+		GS_GOTO_CLEAN();
+
+	if (!!(r = aux_frame_read_size(Packet->data, Packet->dataLength, Offset, &Offset, GS_FRAME_SIZE_LEN, &IgnoreSize)))
+		GS_GOTO_CLEAN();
+
+	if (!!(r = aux_frame_read_oid_vec(Packet->data, Packet->dataLength, Offset, &Offset, oTreelist)))
+		GS_GOTO_CLEAN();
+
+	if (!!(r = clnt_missing_trees(RepositoryT, oTreelist, oMissingTreelist)))
+		GS_GOTO_CLEAN();
+
+clean:
+
+	return r;
+}
+
+int clnt_state_4_noown(
+	git_repository *RepositoryT, ENetHost *host, ENetPeer *peer, ServAuxData *ServAuxData,
+	std::vector<git_oid> *MissingTreelist,
+	std::vector<git_oid> *oMissingBloblist, gs_packet_t *oPacketTree, uint32_t *oOffsetSizeBufferTree, uint32_t *oOffsetObjectBufferTree)
+{
+	int r = 0;
+
+	std::string Buffer;
+	const gs_packet_t &PacketTree = *oPacketTree;
+	uint32_t Offset = 0;
+	uint32_t IgnoreSize = 0;
+
+	uint32_t BufferTreeLen;
+
+	if (!!(r = aux_frame_full_write_request_trees(&Buffer, MissingTreelist)))
+		GS_GOTO_CLEAN();
+	if (!!(r = aux_packet_full_send(host, peer, ServAuxData, Buffer.data(), Buffer.size(), 0)))
+		GS_GOTO_CLEAN();
+
+	/* NOTE: NOALLOC - PacketTree Lifetime start */
+
+	if (!!(r = aux_host_service_one_type_receive(host, GS_RECEIVE_TIMEOUT_MS, oPacketTree)))
+		GS_GOTO_CLEAN();
+
+	if (! PacketTree)
+		GS_ERR_CLEAN(1);
+
+	if (!!(r = aux_frame_ensure_frametype(PacketTree->data, PacketTree->dataLength, Offset, &Offset, GS_FRAME_TYPE_DECL(RESPONSE_TREES))))
+		GS_GOTO_CLEAN();
+
+	if (!!(r = aux_frame_read_size(PacketTree->data, PacketTree->dataLength, Offset, &Offset, GS_FRAME_SIZE_LEN, &IgnoreSize)))
+		GS_GOTO_CLEAN();
+
+	/* NOTE: NOALLOC - PacketTree Offsets use start */
+
+	if (!!(r = aux_frame_full_aux_read_paired_vec_noalloc(PacketTree->data, PacketTree->dataLength, Offset, &Offset,
+		&BufferTreeLen, oOffsetSizeBufferTree, oOffsetObjectBufferTree)))
+	{
+		GS_GOTO_CLEAN();
+	}
+
+	if (!!(r = clnt_missing_blobs_bare(
+		RepositoryT,
+		PacketTree->data, PacketTree->dataLength, *oOffsetSizeBufferTree,
+		PacketTree->data, PacketTree->dataLength, *oOffsetObjectBufferTree, MissingTreelist->size(), oMissingBloblist)))
+	{
+		GS_GOTO_CLEAN();
+	}
+
+clean:
+
+	return r;
+}
+
+int clnt_state_5_noown(
+	git_repository *RepositoryT, ENetHost *host, ENetPeer *peer, ServAuxData *ServAuxData,
+	std::vector<git_oid> *MissingTreelist, std::vector<git_oid> *MissingBloblist,
+	const gs_packet_t &PacketTree, uint32_t OffsetSizeBufferTree, uint32_t OffsetObjectBufferTree,
+	std::vector<git_oid> *oWrittenBlob, std::vector<git_oid> *oWrittenTree)
+{
+	int r = 0;
+
+	std::string Buffer;
+	gs_packet_t PacketBlob;
+	uint32_t Offset = 0;
+	uint32_t IgnoreSize = 0;
+
+	uint32_t BufferBlobLen;
+	uint32_t OffsetSizeBufferBlob;
+	uint32_t OffsetObjectBufferBlob;
+
+	if (!!(r = aux_frame_full_write_request_blobs(&Buffer, MissingBloblist)))
+		GS_GOTO_CLEAN();
+	if (!!(r = aux_packet_full_send(host, peer, ServAuxData, Buffer.data(), Buffer.size(), 0)))
+		GS_GOTO_CLEAN();
+
+	/* NOTE: NOALLOC - PacketBlob Lifetime start */
+
+	if (!!(r = aux_host_service_one_type_receive(host, GS_RECEIVE_TIMEOUT_MS, &PacketBlob)))
+		GS_GOTO_CLEAN();
+
+	if (! PacketBlob)
+		GS_ERR_CLEAN(1);
+
+	if (!!(r = aux_frame_ensure_frametype(PacketBlob->data, PacketBlob->dataLength, Offset, &Offset, GS_FRAME_TYPE_DECL(RESPONSE_BLOBS))))
+		GS_GOTO_CLEAN();
+
+	if (!!(r = aux_frame_read_size(PacketBlob->data, PacketBlob->dataLength, Offset, &Offset, GS_FRAME_SIZE_LEN, &IgnoreSize)))
+		GS_GOTO_CLEAN();
+
+	/* NOTE: NOALLOC - PacketBlob Offsets use start */
+
+	if (!!(r = aux_frame_full_aux_read_paired_vec_noalloc(PacketBlob->data, PacketBlob->dataLength, Offset, &Offset,
+		&BufferBlobLen, &OffsetSizeBufferBlob, &OffsetObjectBufferBlob)))
+	{
+		GS_GOTO_CLEAN();
+	}
+
+	if (!!(r = clnt_deserialize_blobs(
+		RepositoryT,
+		PacketBlob->data, PacketBlob->dataLength, OffsetSizeBufferBlob,
+		PacketBlob->data, PacketBlob->dataLength, OffsetObjectBufferBlob,
+		MissingBloblist->size(), oWrittenBlob)))
+	{
+		GS_GOTO_CLEAN();
+	}
+
+	if (!!(r = clnt_deserialize_trees(
+		RepositoryT,
+		PacketTree->data, PacketTree->dataLength, OffsetSizeBufferTree,
+		PacketTree->data, PacketTree->dataLength, OffsetObjectBufferTree,
+		MissingTreelist->size(), oWrittenTree)))
+	{
+		GS_GOTO_CLEAN();
+	}
+
+clean:
+
+	return r;
+}
+
+int clnt_state_0_setup(const sp<ClntState> &State, const confmap_t &ClntKeyVal, const sp<ServAuxData> &ServAuxData) {
+	int r = 0;
+
+	sp<git_repository *> RepositoryT(new git_repository *);
+
+	const char *ConfRepoTOpenPath = aux_config_key(ClntKeyVal, "ConfRepoTOpenPath");
+
+	if (!ConfRepoTOpenPath)
+		GS_ERR_CLEAN(1);
+
+	if (!!(r = clnt_state_0_noown(ConfRepoTOpenPath, RepositoryT.get())))
+		GS_GOTO_CLEAN();
+
+	GS_CLNT_STATE_CODE_SET_ENSURE_NONUCF(State.get(), 1, a,
+		{ a.mRepositoryT = RepositoryT; });
+
+clean:
+	if (!!r) {
+		if (RepositoryT)
+			git_repository_free(*RepositoryT);
+	}
+
+	return r;
+}
+
+int clnt_state_1_setup(const sp<ClntState> &State, const confmap_t &ClntKeyVal, const sp<ServAuxData> &ServAuxData) {
+	int r = 0;
+
+	sp<gs_host_peer_pair_t> Connection(new gs_host_peer_pair_t(NULL, NULL));
+
+	const char *ConfServHostName = aux_config_key(ClntKeyVal, "ConfServHostName");
+	uint32_t ConfServPort = 0;
+
+	if (!ConfServHostName)
+		GS_ERR_CLEAN(1);
+
+	if (!!(r = aux_config_key_uint32(ClntKeyVal, "ConfServPort", &ConfServPort)))
+		GS_GOTO_CLEAN();
+
+	if (!!(r = clnt_state_1_noown(ConfServPort, ConfServHostName, &Connection->first, &Connection->second)))
+		GS_GOTO_CLEAN();
+
+	GS_CLNT_STATE_CODE_SET_ENSURE_NONUCF(State.get(), 2, a,
+		{ a.mConnection = Connection; });
+
+clean:
+	if (!!r) {
+		if (Connection && Connection->second)
+			enet_peer_disconnect_now(Connection->second, NULL);
+
+		if (Connection && Connection->first)
+			enet_host_destroy(Connection->first);
+	}
+
+	return r;
+}
+
+int clnt_state_2_setup(const sp<ClntState> &State, const confmap_t &ClntKeyVal, const sp<ServAuxData> &ServAuxData) {
+	int r = 0;
+
+	sp<git_oid> TreeHeadOid(new git_oid);
+
+	git_repository * const RepositoryT = *State->mRepositoryT;
+	const sp<gs_host_peer_pair_t> &Connection = State->mConnection;
+	const char *ConfRefName = aux_config_key(ClntKeyVal, "RefName");
+
+	std::string Buffer;
+	gs_packet_t Packet;
+	uint32_t Offset = 0;
+
+	git_oid CommitHeadOidT = {};
+	git_oid TreeHeadOidT = {};
+
+	if (!ConfRefName)
+		GS_ERR_CLEAN(1);
+
+	if (!!(r = clnt_state_2_noown(
+		ConfRefName, RepositoryT, Connection->first, Connection->second, ServAuxData.get(),
+		TreeHeadOid.get())))
+	{
+		GS_GOTO_CLEAN();
+	}
+
+	GS_CLNT_STATE_CODE_SET_ENSURE_NONUCF(State.get(), 3, a,
+		{ a.mTreeHeadOid = TreeHeadOid; });
+
+clean:
+
+	return r;
+}
+
+int clnt_state_3_setup(const sp<ClntState> &State, const confmap_t &ClntKeyVal, const sp<ServAuxData> &ServAuxData) {
+	int r = 0;
+
+	sp<std::vector<git_oid> > Treelist(new std::vector<git_oid>);
+	sp<std::vector<git_oid> > MissingTreelist(new std::vector<git_oid>);
+
+	git_repository * const RepositoryT = *State->mRepositoryT;
+	const sp<gs_host_peer_pair_t> &Connection = State->mConnection;
+	const sp<git_oid> &TreeHeadOid = State->mTreeHeadOid;
+
+	if (!!(r = clnt_state_3_noown(
+		RepositoryT, Connection->first, Connection->second, ServAuxData.get(),
+		TreeHeadOid.get(), Treelist.get(), MissingTreelist.get())))
+	{
+		GS_GOTO_CLEAN();
+	}
+
+	GS_CLNT_STATE_CODE_SET_ENSURE_NONUCF(State.get(), 4, a,
+		{ a.mTreelist = Treelist;
+		  a.mMissingTreelist = MissingTreelist; });
+
+clean:
+
+	return r;
+}
+
+int clnt_state_4_setup(const sp<ClntState> &State, const confmap_t &ClntKeyVal, const sp<ServAuxData> &ServAuxData) {
+	int r = 0;
+
+	sp<std::vector<git_oid> > MissingBloblist(new std::vector<git_oid>);
+	sp<PacketWithOffset> PacketTreeWithOffset(new PacketWithOffset);
+
+	git_repository * const RepositoryT = *State->mRepositoryT;
+	const sp<gs_host_peer_pair_t> &Connection = State->mConnection;
+	const sp<std::vector<git_oid> > &MissingTreelist = State->mMissingTreelist;
+
+	gs_packet_t PacketTree;  // FIXME: NULL SHARED_PTR GETS ASSIGNED LATER
+
+	uint32_t OffsetSizeBufferTree;
+	uint32_t OffsetObjectBufferTree;
+
+	PacketWithOffset TmpTreePacketWithOffset = {};
+
+	if (!!(r = clnt_state_4_noown(
+		RepositoryT, Connection->first, Connection->second, ServAuxData.get(),
+		MissingTreelist.get(), MissingBloblist.get(), &PacketTree, &OffsetSizeBufferTree, &OffsetObjectBufferTree)))
+	{
+		GS_GOTO_CLEAN();
+	}
+
+	if (!!(r = aux_make_packet_with_offset(PacketTree, OffsetSizeBufferTree, OffsetObjectBufferTree, &TmpTreePacketWithOffset)))
+		GS_GOTO_CLEAN();
+
+	GS_CLNT_STATE_CODE_SET_ENSURE_NONUCF(State.get(), 5, a,
+		{ a.mMissingBloblist = MissingBloblist;
+		  *a.mTreePacketWithOffset = TmpTreePacketWithOffset; });
+
+clean:
+
+	return r;
+}
+
+int clnt_state_5_setup(const sp<ClntState> &State, const confmap_t &ClntKeyVal, const sp<ServAuxData> &ServAuxData) {
+	int r = 0;
+
+	sp<std::vector<git_oid> > WrittenBlob(new std::vector<git_oid>);
+	sp<std::vector<git_oid> > WrittenTree(new std::vector<git_oid>);
+
+	git_repository * const RepositoryT = *State->mRepositoryT;
+	const sp<gs_host_peer_pair_t> &Connection = State->mConnection;
+	const sp<std::vector<git_oid> > &MissingTreelist = State->mMissingTreelist;
+	const sp<std::vector<git_oid> > &MissingBloblist = State->mMissingBloblist;
+	const sp<PacketWithOffset> &PacketTreeWithOffset = State->mTreePacketWithOffset;
+	const gs_packet_t &PacketTree = PacketTreeWithOffset->mPacket;
+	const uint32_t &OffsetSizeBufferTree = PacketTreeWithOffset->mOffsetSize;
+	const uint32_t &OffsetObjectBufferTree = PacketTreeWithOffset->mOffsetObject;
+
+	if (!!(r = clnt_state_5_noown(
+		RepositoryT, Connection->first, Connection->second, ServAuxData.get(),
+		MissingTreelist.get(), MissingBloblist.get(),
+		PacketTree, OffsetSizeBufferTree, OffsetObjectBufferTree,
+		WrittenBlob.get(), WrittenTree.get())))
+	{
+		GS_GOTO_CLEAN();
+	}
+
+	GS_CLNT_STATE_CODE_SET_ENSURE_NONUCF(State.get(), 6, a,
+		{ a.mWrittenBlob = WrittenBlob;
+		  a.mWrittenTree = WrittenTree; });
+
+clean:
+
+	return r;
+}
+
+int clnt_state_crank(const sp<ClntState> &State, const confmap_t &ClntKeyVal, const sp<ServAuxData> &ServAuxData) {
+	int r = 0;
+
+	uint32_t Code = 0;
+
+	if (!!(r = clnt_state_code(State.get(), &Code)))
+		GS_GOTO_CLEAN();
+
+	switch (Code) {
+	case 0:
+	{
+		if (!!(r = clnt_state_0_setup(State, ClntKeyVal, ServAuxData)))
+			GS_GOTO_CLEAN();
+	}
+	break;
+
+	case 1:
+	{
+		if (!!(r = clnt_state_1_setup(State, ClntKeyVal, ServAuxData)))
+			GS_GOTO_CLEAN();
+	}
+	break;
+
+	case 2:
+	{
+		if (!!(r = clnt_state_2_setup(State, ClntKeyVal, ServAuxData)))
+			GS_GOTO_CLEAN();
+	}
+	break;
+
+	case 3:
+	{
+		if (!!(r = clnt_state_3_setup(State, ClntKeyVal, ServAuxData)))
+			GS_GOTO_CLEAN();
+	}
+	break;
+
+	case 4:
+	{
+		if (!!(r = clnt_state_4_setup(State, ClntKeyVal, ServAuxData)))
+			GS_GOTO_CLEAN();
+	}
+	break;
+
+	case 5:
+	{
+		if (!!(r = clnt_state_5_setup(State, ClntKeyVal, ServAuxData)))
+			GS_GOTO_CLEAN();
+	}
+	break;
+
+	default:
+	{
+		assert(0);
+	}
+	break;
+	}
+
+clean:
+
+	return r;
+}
+
 int clnt_thread_func(const confmap_t &ClntKeyVal, sp<ServAuxData> ServAuxData) {
+	int r = 0;
+
+	sp<ClntState> State(new ClntState);
+
+	while (true) {
+
+		if (!!(r = clnt_state_crank(State, ClntKeyVal, ServAuxData)))
+			GS_GOTO_CLEAN();
+
+	}
+
+clean:
+
+	return r;
+}
+
+int clnt_thread_func____(const confmap_t &ClntKeyVal, sp<ServAuxData> ServAuxData) {
 	int r = 0;
 
 	ENetAddress address = {};
